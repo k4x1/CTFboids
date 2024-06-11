2cb8924e563ec920e3f8e93bbe295d15fc534309
k4x1 (coolest guy) <kazuoandrade@gmail.com>
2024-06-11 17:31:09 +1200

    started workl on team system

--- Assets/scripts/boidManager.cs
+++ Assets/scripts/boidManager.cs
@@ -1,21 +1,91 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+
+[System.Serializable]
+public struct Goal
+{
+    public float weight;
+    public byte team;
+    public GameObject obj;
+    public string name;
+}
 
 public class boidManager : MonoBehaviour
 {
     public List<GameObject> m_redTeam;
     public List<GameObject> m_bluTeam;
+    public List<GameObject> m_taggable;
+    public List<GameObject> m_boids;
+    public List<Goal> m_goals;
 
-    void Update()
+    private void Start()
     {
-        for(int i = 0; i < m_redTeam.Count; i++) { 
-            
-        }
-        for (int i = 0; i < m_bluTeam.Count; i++)
-        {
-
+        foreach (GameObject go in m_boids) { 
+            if(Mathf.Sign(go.transform.position.x) == 1)
+            {
+                go.GetComponent<boids>().team = 0;
+                go.GetComponent<SpriteRenderer>().color = Color.red;
+                m_redTeam.Add(gameObject);
+            }
+            else
+            {
+                go.GetComponent<boids>().team = 1;
+                go.GetComponent<SpriteRenderer>().color = Color.blue;
+                m_bluTeam.Add(gameObject);
+            }
         }
     }
-}
+    private void Update()
+    {
+        for(int i = 0; i< m_redTeam.Count; i++)
+        {
+            if (m_redTeam[i].GetComponent<boids>().m_jailed)
+            {
 
+            }
+        }
+    }
+    public Goal? GetRandomGoal(byte team)
+    {
+        // Filter goals by the specified team
+        List<Goal> filteredGoals = new List<Goal>();
+        foreach (var goal in m_goals)
+        {
+            if (goal.team == team)
+            {
+                filteredGoals.Add(goal);
+            }
+        }
+
+        // If no goals are found for the specified team, return null
+        if (filteredGoals.Count == 0)
+        {
+            return null;
+        }
+
+        // Calculate the total weight of the filtered goals
+        float totalWeight = 0f;
+        foreach (var goal in filteredGoals)
+        {
+            totalWeight += goal.weight;
+        }
+
+        // Generate a random number within the range of the total weight
+        float randomWeight = Random.Range(0f, totalWeight);
+
+        // Select the goal based on the random weight
+        float cumulativeWeight = 0f;
+        foreach (var goal in filteredGoals)
+        {
+            cumulativeWeight += goal.weight;
+            if (randomWeight <= cumulativeWeight)
+            {
+                return goal;
+            }
+        }
+
+        // Fallback in case something goes wrong (shouldn't happen)
+        return filteredGoals[filteredGoals.Count - 1];
+    }
+}
--- Assets/scripts/boids.cs
+++ Assets/scripts/boids.cs
@@ -4,18 +4,20 @@ using System.Collections.Generic;
 using Unity.Burst.CompilerServices;
 using UnityEngine;
 
-
 public class boids : MonoBehaviour
 {
     [SerializeField] float m_avoidanceStr = 2;
     [SerializeField] float m_destinationStr = 1;
+    [SerializeField] float m_maxAvoidanceStr = 10;
     //[SerializeField]
     public Vector2 m_direction;
     public int m_detectionRes = 8;
     public RaycastHit2D[][] m_hitArray;
     public float m_avoidanceRange = 1;
     public float m_speed = 10;
-    
+
+    public bool m_jailed = false;
+    public bool m_taggable = false;
 
     public byte team = 0;
 
@@ -23,52 +25,63 @@ public class boids : MonoBehaviour
 
     private Rigidbody2D m_rb;
     private BoxCollider2D m_collider;
-    private GameObject m_boidManagerRef;
+    private boidManager m_boidManagerRef;
 
     [SerializeField] private Vector3 m_avoidance;
-   [SerializeField] private Vector3 m_destination;
+    [SerializeField] private Vector3 m_destination;
+
     void Start()
     {
-        m_boidManagerRef = GameObject.FindWithTag("boidManager");
+        m_boidManagerRef = GameObject.FindWithTag("boidManager").GetComponent<boidManager>();
 
-       
         m_collider = GetComponent<BoxCollider2D>();
         m_rb = GetComponent<Rigidbody2D>();
 
         if (team == 0)
         {
             GetComponent<SpriteRenderer>().color = Color.red;
-            m_boidManagerRef.GetComponent<boidManager>().m_redTeam.Add(gameObject);
+            m_boidManagerRef.m_redTeam.Add(gameObject);
         }
         else
         {
             GetComponent<SpriteRenderer>().color = Color.blue;
-            m_boidManagerRef.GetComponent<boidManager>().m_bluTeam.Add(gameObject);
+            m_boidManagerRef.m_bluTeam.Add(gameObject);
         }
     }
+
     void Update()
     {
-        
-        m_avoidance = Vector2.zero;
+        checkTeamSpecific();
+        ResetVectors();
+        PerformRaycasting();
+        CalculateDestination();
+        UpdateVelocity();
+        RotateBoid();
+    }
+
+    void ResetVectors()
+    {
         m_destination = Vector2.zero;
+        m_avoidance = Vector2.zero;
+    }
+
+    void PerformRaycasting()
+    {
         m_hitArray = new RaycastHit2D[m_detectionRes][];
         float rayDirectionDiv = 360 / m_detectionRes;
 
         for (int i = 0; i < m_detectionRes; i++)
         {
-            // Calculate the angle in radians
             float angle = Mathf.Deg2Rad * (rayDirectionDiv * i);
-
-            // Calculate the direction vector based on the angle
             Vector2 rayDirection = new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
-
             m_hitArray[i] = Physics2D.RaycastAll(transform.position, rayDirection, m_avoidanceRange);
 
             bool collided = false;
             Vector3 otherPos = Vector2.zero;
-            foreach(RaycastHit2D hit in m_hitArray[i]) { 
+            foreach (RaycastHit2D hit in m_hitArray[i])
+            {
                 Collider2D collider = hit.collider;
-                if(collider == m_collider || collider.gameObject == m_destinationObj || collider.gameObject.CompareTag("noAvoid"))
+                if (collider == m_collider || collider.gameObject == m_destinationObj || collider.gameObject.CompareTag("noAvoid"))
                 {
                     continue;
                 }
@@ -83,28 +96,63 @@ public class boids : MonoBehaviour
             {
                 Debug.DrawRay(transform.position, rayDirection * m_avoidanceRange, Color.red);
                 float dist = Vector2.Distance(transform.position, otherPos);
-                float inverseDist = (dist > 0 ? 1.0f / (dist) : float.MaxValue) ;
+                float inverseDist = (dist > 0 ? (-(m_maxAvoidanceStr / m_avoidanceRange) * dist) + m_maxAvoidanceStr : float.MaxValue);
                 m_avoidance = m_avoidance - (Vector3.Normalize(otherPos - transform.position) * inverseDist);
-                Debug.Log(inverseDist);
             }
             else
             {
-
                 Debug.DrawRay(transform.position, rayDirection * m_avoidanceRange, Color.green);
             }
         }
+    }
 
-        m_destination = Vector3.Normalize(m_destinationObj.transform.position - transform.position); 
+    void CalculateDestination()
+    {
+        m_destination = Vector3.Normalize(m_destinationObj.transform.position - transform.position);
+    }
 
+    void UpdateVelocity()
+    {
+        m_rb.velocity = (new Vector3(1, 1, 0)) + Vector3.Normalize((m_avoidance * m_avoidanceStr) + (m_destination * m_destinationStr)) * m_speed;
+    }
 
-
-        m_rb.velocity = (new Vector3(1,1,0)) + Vector3.Normalize(   (m_avoidance * m_avoidanceStr) +  (m_destination * m_destinationStr)    ) * m_speed;
-
+    void RotateBoid()
+    {
         if (m_rb.velocity != Vector2.zero)
         {
             float angle = Mathf.Atan2(m_rb.velocity.y, m_rb.velocity.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle-90));
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle - 90));
         }
     }
 
+    void checkTeamSpecific()
+    {
+        if (team == 0)
+        {
+            // Team-specific logic for team 0
+        }
+        else
+        {
+            // Team-specific logic for other teams
+        }
+
+        if (m_taggable)
+        {
+            if ((team == 0 && Mathf.Sign(transform.position.x) == 1) ||
+                (team != 0 && Mathf.Sign(transform.position.x) == -1))
+            {
+                m_taggable = false;
+                m_boidManagerRef.m_taggable.Remove(gameObject);
+            }
+        }
+        else
+        {
+            if ((team == 0 && Mathf.Sign(transform.position.x) == -1) ||
+               (team != 0 && Mathf.Sign(transform.position.x) == 1))
+            {
+                m_taggable = true;
+                m_boidManagerRef.m_taggable.Add(gameObject);
+            }
+        }
+    }
 }
